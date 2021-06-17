3489aee588ab520c0190b3c100221516991fe137
MeghaManwal <https://github.com/MeghaManwal/Basic-Calculator.git>
2021-06-17 10:12:51 +0530

    Added:To return JSON object using Greeting Controllers

---
+++ GreetingAppDevelopment/src/test/java/com/greeting/controllers/GreetingAppDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greeting.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingAppDevelopmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
