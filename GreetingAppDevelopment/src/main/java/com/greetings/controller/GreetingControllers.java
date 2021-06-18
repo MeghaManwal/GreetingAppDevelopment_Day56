6bb465d3b0c680560a53547c016eb2a99c8d28b9
MeghaManwal <https://github.com/MeghaManwal/Basic-Calculator.git>
2021-06-18 10:58:29 +0530

    Added:To get the different messages using Service Layer

--- GreetingAppDevelopment/src/main/java/com/greetings/controller/GreetingControllers.java
+++ GreetingAppDevelopment/src/main/java/com/greetings/controller/GreetingControllers.java
@@ -3,6 +3,10 @@ package com.greetings.controller;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +31,16 @@ public class GreetingControllers {
 		user.setFirstName(name);
 		return greetingService.addGreeting(user);
 		
-	}	
+	}
+	
+	@PostMapping("/post")
+	public String sayHello(@RequestBody User user) {
+		return "Hello " + user.getFirstName() + " " + user.getLastName() + " !!";
+	}
 
+	@PutMapping("/put/{firstName}")
+	public String sayHello(@PathVariable String firstName,
+			               @RequestParam(value="lastName") String lastName) {
+		return "Hello" + firstName +" "+ lastName + " !!";
+	}
 }
